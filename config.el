fd0e59300f9fe10032d0b16f93875c7f10381380
Anish Moorthy <amoorthy@google.com>
2021-09-24 15:42:18 +0000

    Make init.el the entry point

--- config.el
+++ config.el
@@ -1,4 +1,1 @@
 ;;; config.el --- Description -*- lexical-binding: t; -*-
-
-(load-local-file "packages.el")
-(load-local-file "keybindings.el")
--- init.el
+++ init.el
@@ -1,1 +1,5 @@
 ;;; init.el --- Description -*- lexical-binding: t; -*-
+
+(load-local-file "packages.el")
+(load-local-file "config.el")
+(load-local-file "keybindings.el")
